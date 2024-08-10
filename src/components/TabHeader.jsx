ae5421be62cb8c60987aa83170e9c30b522467af
Julian Florez <forezjuli08@hotmail.com>
2024-08-10 13:32:12 -0500

    update - use map to generate a list of items

--- src/components/TabHeader.jsx
+++ src/components/TabHeader.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-// import classes from './TabHeader.module.css'
+import React, { useState } from 'react';
 import './TabHeader.module.css'
 
 function TabHeader({ active, setActiveTab }) {
   // const { departments, regions } = useContext(GlobalDataContext)
+  const [listItemsInfo, setListItemsInfo] = useState([
+    { presidentes: "Presidentes" },
+    { aeropuertos: "Aeropuertos" },
+    { atracciones: "Atracciones turísticas" },
+  ])
 
   return (
     <React.Fragment>
       <ul>
-        <li className={active === 'presidentes' ? 'tabactive' : 'tabinactive'} onClick={() => setActiveTab('presidentes')}>Presidentes</li>
-        <li className={active === 'aeropuertos' ? 'tabactive' : 'tabinactive'} onClick={() => setActiveTab('aeropuertos')}>Aeropuertos </li>
-        <li className={active === 'atracciones' ? 'tabactive' : 'tabinactive'} onClick={() => setActiveTab('atracciones')}>Atracciones turísticas</li>
+        {
+          listItemsInfo.map((item) => {
+            const [key, value] = Object.entries(item)[0]
+            return (
+              <li key={key + value} className={active === key ? 'tabactive' : 'tabinactive'} onClick={() => setActiveTab(key)}>{value}</li>
+            )
+          })
+        }
       </ul>
-      <p>{active}</p>
+      {/* <p>{active}</p> */}
     </React.Fragment>
   )
 }
